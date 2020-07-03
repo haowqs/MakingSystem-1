0f982ff0cadded30d74c99b1e68f4e6f0bb7d402
haowqs <hw121298@163.com>
2020-07-03 11:14:59 +0800

    Update config.py

--- makingsystem/settings/config.py
+++ makingsystem/settings/config.py
@@ -1,25 +1,3 @@
-
-"""
-阿里云 对象存储OSS 配置
-https://oss.console.aliyun.com/overview
-创建用户 赋予AMS OSS访问权限
-https://help.aliyun.com/document_detail/31837.html?spm=a2c4g.11186623.2.12.3e936c40MumyJx#concept-zt4-cvy-5db
-"""
-
-AccessKeyId = 'LTAI5wtrKrrWNZyV' # 新建用户时可以查看
-AccessKeySecret = 'h2p8XTFJYSZx3FDYx53auPDnMquI8R' # 新建用户时可以查看
-Endpoint = 'oss-cn-beijing.aliyuncs.com'# 节点名 例如 oss-cn-beijing.aliyuncs.com
-bucketName = 'liderar'# 存储空间名
-
-"""
-
-用户登录名称 liderar@1331305356221389.onaliyun.com
-AccessKey ID LTAI5wtrKrrWNZyV
-AccessKeySecret h2p8XTFJYSZx3FDYx53auPDnMquI8R
-
-"""
-
-
 """
 腾讯云 短信
 """
